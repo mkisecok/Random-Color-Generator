ecc51e626a6082565fe15087441aa51eda991427
Mkisecok <mahmutkisecok@gmail.com>
2021-11-10 19:53:11 +0100

    Add if statement for remove-button

--- index.js
+++ index.js
@@ -11,49 +11,60 @@ function getRandomColor() {
   
   
   function setRandomColor(divCount)
-  { for(let i=1 ; i<divCount; i++)
+  { for(let i=0 ; i<divCount; i++)
     {
       const newDiv=document.createElement('div');
 
-      newDiv.classList.add('muster-box','m-4','d-flex', 'justify-content-center' );
+      newDiv.classList.add('muster-box','m-4','d-flex' , 'flex-row-reverse' );
 
       newDiv.style.background=getRandomColor()
       
       const newButton=document.createElement('button');
 
-      newButton.classList.add('remove-button', 'align-self-end')
+      newButton.classList.add('remove-button' )
 
+      newButton.innerHTML='X'
       const newPar=document.createElement('p');
 
       newPar.classList.add('hex-code', 'card-text', 'text-light', 'align-self-center')
 
       const newText= document.createTextNode(getRandomColor());
-console.log(newDiv);
+
       newDiv.appendChild(newButton)
       newDiv.appendChild(newPar)
       newPar.appendChild(newText)
-     
       colorContainer.append(newDiv)
 
     }
     
   }
-
+  const main =document.querySelector('.main-container');
   const addButton=document.querySelector('#add-button');
-  const musterBox=document.querySelector('.muster-box');
+  
   const colorCont=document.querySelector('.color-container');
   const input = document.querySelector('#valueInput');
   const colorContainer=document.querySelector('#color-container');
-
+  
   let countBox;
-  document.addEventListener('click', e=>{
+  
+  main.addEventListener('click', e=>{
 
     if(e.target == addButton)
     {
         countBox = input.value;
         
-      
+        setRandomColor(countBox)
     }
-    setRandomColor(countBox)
+
+    if(e.target.matches('.remove-button'))
+    {
+        
+        e.path[1].remove();
+         input.value-- 
+        
+    }
     
   })
+  
+  
+ 
